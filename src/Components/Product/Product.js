d3558e45cb7c6ac5ee830e615c6ab99c4e803f81
Meraj <merajulhasan150@gmail.com>
2022-09-29 21:00:01 +0600

    3rd commit-product.js

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Product.css';
 const Product = (props) => {
- 
+ const {name,age,img,time}=props.product; 
+ const{handleAddToCart,product}=props;
     return (
         <div className='product_container'>
-         
+            <img src={img} alt=''/>
+           <div className='product_info'>
+              <p className='product_name'>{name}</p>
+              <p>Age: {age}</p>
+              <p>Time-Reqiure: {time}</p>
+           </div> 
+           
+         <button  onClick={()=>handleAddToCart(product)} className='btn_cart'>
+             <p className='btn_text'>Add To List</p>
+         </button>
 
         </div>
     );
