905e2f29c4cf111f55e014eb166900297568c2f0
Meraj <merajulhasan150@gmail.com>
2022-09-29 20:57:23 +0600

    2nd Commit

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,20 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
 import './Shop.css';
-const Shop = () => {
 
+  const Shop = () => {
+   const [products,setProducts]=useState([]);
 
-
-const addTimeBreak=(breakTime)=>{
- 
-}
-
-
-
-
-    return (
-        <div className='shop_conatiner'>
-        
-       </div>
-    );
+   return (
+    <div className='shop_conatiner'>
+     <div className="products_container">
+        {
+          products.map(product=><Product
+           key={product._id}
+           product={product}
+          
+          ></Product>)
+        }
+     </div>
+     <div className="cart-container">
+      <Cart
+      
+      ></Cart>
+     </div>
+   </div>
+);
 };
 
 export default Shop;
